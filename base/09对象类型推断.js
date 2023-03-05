7ccdd1b2421547cdd43d58a67f8ff003bdcd37af
EnoshDong <enoshdong@qq.com>
2023-03-05 15:25:37 +0800

    TypeScript object type inference.

---
+++ base/09对象类型推断.js
@@ -0,0 +1,15 @@
+var user = {
+    name: 'hello',
+    age: 20,
+    open: true,
+    lesson: [
+        { title: 'linux' },
+        { title: 'mac' },
+    ]
+};
+user.age = 30;
+// user.age = '30';
+user.open = false;
+user.lesson.push({ title: 'windows' });
+console.log('user:', user);
+console.log('user.lesson:', user.lesson[0].title);
